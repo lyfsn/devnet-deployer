df5d54b01520aa5c44f7838eb3977a4adabbce76
wangyufsn <dev.wangyu@proton.me>
2023-11-21 15:02:16 +0800

    change genesis method

--- genesis.sh
+++ genesis.sh
@@ -1,9 +1,32 @@
 
 rm -rvf el-cl-genesis-data
 
+docker run \
+  -it \
+  -v $(pwd)/el-cl-genesis-data:/data \
+  -v $(pwd)/config-example:/config \
+  -u $UID \
+  --rm \
+  wangyufsn/ethereum-genesis-generator:2.0.4 all
 
-docker compose -f compose-genesis.yaml up 
-docker compose -f compose-genesis.yaml down 
+
+# docker run \
+#   -it \
+#   --env http_proxy=http://host.docker.internal:7890 \
+#   --env https_proxy=http://host.docker.internal:7890 \
+#   --env all_proxy=socks5://host.docker.internal:7890 \
+#   -v $(pwd)/el-cl-genesis-data:/data \
+#   -v $(pwd)/config-example:/config \
+#   -u $UID \
+#   --rm \
+#   wangyufsn/ethereum-genesis-generator:2.0.4 all
+
+
+
+
+
+
+
 
 
 
--- initGeth.sh
+++ initGeth.sh
@@ -1,8 +1,13 @@
 
 
-# rm -rvf execution-data
-
-docker compose -f compose-init-geth.yaml up 
-docker compose -f compose-init-geth.yaml down
-
+docker run \
+  -it \
+  -v $(pwd)/execution-data:/execution-data \
+  -v $(pwd)/config-example:/config \
+  -v $(pwd)/el-cl-genesis-data:/el-cl-genesis-data \
+  ethereum/client-go:v1.13.4 \
+  --state.scheme=path \
+  --datadir=/execution-data \
+  init \
+  /el-cl-genesis-data/custom_config_data/genesis.json
 
