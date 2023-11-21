e30475bcf73632bd61f438a787eb50303918f06d
wangyufsn <dev.wangyu@proton.me>
2023-11-21 19:43:51 +0800

    up

--- genesis.local.sh
+++ genesis.local.sh
@@ -1,8 +1,12 @@
 
 rm -rvf el-cl-genesis-data
 
+
 docker run \
   -it \
+  --env http_proxy=http://host.docker.internal:7890 \
+  --env https_proxy=http://host.docker.internal:7890 \
+  --env all_proxy=socks5://host.docker.internal:7890 \
   -v $(pwd)/el-cl-genesis-data:/data \
   -v $(pwd)/config-example:/config \
   --rm \
