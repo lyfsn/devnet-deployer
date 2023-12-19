815aed459ce2f0074f211e398a35abf5f24952a1
lyfsn <dev.wangyu@proton.me>
2023-12-19 16:45:35 +0800

    up

---
+++ start.devnet2.sh
@@ -0,0 +1,13 @@
+
+mkdir -p config-example
+mkdir -p el-cl-genesis-data
+mkdir -p execution-data
+mkdir -p consensus-data
+mkdir -p validator-keys
+
+
+export IP_ADDRESS=78.46.91.61
+docker compose -f compose.yaml up -d
+
+
+
